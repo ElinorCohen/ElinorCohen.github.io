4072fc01ef619625bc4fdfd3e146e08570fe12c6
Elinor <elinorco24@gmail.com>
2024-01-18 14:18:50 +0200

    add mail sending

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
-require("dotenv").config();
-
+const bodyParser = require("body-parser");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 // Require the router module
 const router = require("./routes");
 
 // Use the router
-app.use("/", router);
+app.use(cors({ "*": true, credentials: true }));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api", router);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(8000, () => console.log("Example app is listening on port 8000."));
